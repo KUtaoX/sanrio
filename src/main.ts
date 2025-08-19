d49fc34cbf51a97a74694523ebfcb2df3718d866
KUtaoX <lemonade2546@gmail.com>
2025-08-20 03:24:25 +0700

    add createAccount by kaotu

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
 import './style.css'
 
+const input = document.getElementById("password") as HTMLInputElement | null;
+const icon = document.getElementById("eye-icon");
+
+function togglePassword() {
+  if (!input || !icon) return;
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.innerHTML = `
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-5.523 0-10-4.477-10-10a9.965 9.965 0 011.175-4.875"/>
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16.24 7.76a6 6 0 00-8.48 8.48"/>
+    `; 
+  } else {
+    input.type = "password";
+    icon.innerHTML = `
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+    `; 
+  }
+}
